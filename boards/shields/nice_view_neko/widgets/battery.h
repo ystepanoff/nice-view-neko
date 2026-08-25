1327d7370712c0fd7ff60c94ae87f5cdf552459e
Yegor Stepanov <ystepanoff@icloud.com>
2026-08-25 21:23:11 +0100

    fix: define missing structs

--- boards/shields/nice_view_neko/widgets/battery.h
+++ boards/shields/nice_view_neko/widgets/battery.h
@@ -7,4 +7,11 @@
 #include <lvgl.h>
 #include "util.h"
 
+struct battery_status_state {
+    uint8_t level;
+#if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
+    bool usb_present;
+#endif
+};
+
 void draw_battery_status(lv_obj_t *canvas, const struct status_state *state);
